e6093d2227705b327ab160af3e263ef2c00d4daa
gsabinos <guilherme.sabinos@gmail.com>
2026-05-14 00:18:45 -0300

    REMOVIDOS TESTES

--- aergia.tab/Ferramentas.panel/Teste.pushbutton/script.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-__title__ = "Teste"
-__doc__ = "Um botão de teste para verificar se a extensão carregou."
-
-from pyrevit import forms
-
-forms.alert("A extensão Aergia foi carregada com sucesso!", title="Aergia Extension")
